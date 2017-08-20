457064fceba819b7094d7982024e9c55f92b64c0
HIGASHI Taiju <higashi@taiju.info>
2017-08-21 06:56:10 +0900

    init

---
+++ FaviconClock.elm
@@ -0,0 +1,95 @@
+port module FaviconClock exposing (..)
+
+import Html exposing (Html, programWithFlags)
+import Collage exposing (collage, text)
+import Element exposing (toHtml)
+import Color exposing (black)
+import Text exposing (fromString, style, Style)
+import Time as T exposing (Time)
+import Date as D
+import Maybe exposing (Maybe(Just))
+
+-- CONSTANTS
+canvasWidth : Int
+canvasWidth = 16
+
+canvasHeight : Int
+canvasHeight = 16
+
+fontSize : Float
+fontSize = 14
+
+textStyle : Style
+textStyle = { typeface = ["monospace"]
+            , height = Just fontSize
+            , color = black
+            , bold = False
+            , italic = False
+            , line = Nothing
+            }
+
+-- PORTS
+port updateFavicon : Int -> Cmd msg
+
+-- UTILITY
+fromTimeToHour : Time -> Int
+fromTimeToHour = D.fromTime >> D.hour
+
+fromTimeToMinute : Time -> Int
+fromTimeToMinute = D.fromTime >> D.minute
+
+fromTimeTo : String -> (Time -> Int)
+fromTimeTo cltype =
+  case cltype of
+    "min" -> fromTimeToMinute
+    "hour" -> fromTimeToHour
+    _ -> fromTimeToMinute
+
+-- FLAG
+type alias Flag = Model
+
+-- MODEL
+type alias Model =
+  { cltype : String
+  , time : Time
+  }
+
+-- MESSAGE
+type Msg = Tick Time
+
+-- INIT
+init : Flag -> (Model, Cmd Msg)
+init flag = (flag, Cmd.none)
+
+-- UPDATE
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+      Tick time ->
+        { model | time = time } ! [ updateFavicon <| (fromTimeTo model.cltype) time ]
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+      collage canvasWidth canvasHeight [(fromTimeTo model.cltype) model.time
+                                       |> toString
+                                       |> fromString
+                                       |> style textStyle
+                                       |> text
+                                       ]
+        |> toHtml
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  T.every T.second Tick
+
+-- MAIN
+main : Program Flag Model Msg
+main =
+  programWithFlags
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
---
+++ README.md
@@ -0,0 +1,40 @@
+# Elm Favicon Clock
+
+## これは何
+
+favicon に分と秒を表示する時計のようなもの。
+
+Google Chrome で、○時の favicon と、○分の favicon を両方タブ固定して使う。
+
+## 使い方
+
+下記 URL にアクセスすると、タイトルと favicon に時を表示する。
+
+- [hour](https://taiju.github.io/elm-favicon-clock/#hour)
+
+下記 URL にアクセスすると、タイトルと favicon に分を表示する。
+
+- [min](https://taiju.github.io/elm-favicon-clock/#min)
+
+以下は `22:52` の例。
+
+![22:52](https://taiju.github.io/elm-favicon-clock/artwork/artwork.png)
+
+## ビルド
+
+```sh
+$ elm-make FaviconClock.elm --output=favicon-clock.js
+```
+
+## 作った動機
+
+Google Chrome のタブ固定機能で固定したタブで時間が確認できると、ブラウザフルスクリーン作業時に捗るかなと思っ
+たのと、Elm で何か書いて見たかったのとが重なったので書いた。
+
+## 反省点
+
+favicon の更新みたいなことは Elm 向きではなかった。
+
+## 制限事項
+
+おおよそ何時何分を把握することは問題ないが、秒単位での精度が高くない。
